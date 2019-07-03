f79246ba236dd1de7f54967a481d9ba50f5228a9
Luke Menard <lukemenard@gmail.com>
2019-07-03 19:00:47 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,6 @@
+$status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   phrase = "It's-a me, Mario!"
   puts phrase
 end
@@ -7,3 +8,4 @@ end
 def toadstool
   puts status
 end
+
